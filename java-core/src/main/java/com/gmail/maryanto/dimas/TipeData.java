89260d5142734c9e3c6c663ba4634eea6501635e
dimasm93 <software.dimas_m@icloud.com>
2020-02-10 09:57:13 +0700

    init project

---
+++ java-core/src/main/java/com/gmail/maryanto/dimas/TipeData.java
@@ -0,0 +1,9 @@
+package com.gmail.maryanto.dimas;
+
+public class TipeData {
+
+    public static void main(String[] args) {
+        Integer umur = 20;
+        System.out.println("Umur saya adalah " + umur);
+    }
+}
