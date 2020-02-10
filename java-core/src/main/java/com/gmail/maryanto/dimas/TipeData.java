a66e370b945c3d69d1db5508917774ec536c506e
dimasm93 <software.dimas_m@icloud.com>
2020-02-10 13:10:56 +0700

    args

--- java-core/src/main/java/com/gmail/maryanto/dimas/TipeData.java
+++ java-core/src/main/java/com/gmail/maryanto/dimas/TipeData.java
@@ -1,24 +1,25 @@
 package com.gmail.maryanto.dimas;
 
-
-import java.time.LocalDate;
-
-
 public class TipeData {
 
-    public static Integer hitungPajak(){
-        return 10 * 10;
+    public static Integer hitungPajak(
+            Integer bil1,
+            Integer bil2) {
+        return bil1 * bil2;
     }
 
     public static void main(String[] args) {
-        String namaLengkap = nama();
-        System.out.println("Nama lengkap saya adalah "+ namaLengkap);
+        String namaLengkap = nama(args);
+        System.out.println("Nama lengkap saya adalah " + namaLengkap);
 
-        Integer pajak = hitungPajak();
-        System.out.println("nilai pajak "+ pajak);
+        Integer pajak = hitungPajak(10, 100);
+        System.out.println("nilai pajak " + pajak);
     }
 
-    public static String nama(){
+    public static String nama(String[] args) {
+        for (String data : args) {
+            System.out.println(data);
+        }
         return "Dimas Maryanto";
     }
 }
