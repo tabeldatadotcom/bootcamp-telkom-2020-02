915b1b2b7760398888e639e7e3da778dd648c520
dimasm93 <software.dimas_m@icloud.com>
2020-02-10 14:47:47 +0700

    maps

--- java-core/src/main/java/com/gmail/maryanto/dimas/ArrayData.java
+++ java-core/src/main/java/com/gmail/maryanto/dimas/ArrayData.java
@@ -5,19 +5,19 @@ import java.util.*;
 public class ArrayData {
 
     public static void main(String[] args) {
-//        List<String> list = new ArrayList<>(
-//                Arrays.asList(
-//                        "Dimas", "Yusuf", "Abdul", "Rega"
-//                )
-//        );
-//
-//        list.add("Test");
-//        String nama = list.get(0);
-//        System.out.println("nama dari index ke 0: " + nama);
-//
-//        for (String s : list) {
-//            System.out.println("nama: " + s);
-//        }
+        List<String> list = new ArrayList<>(
+                Arrays.asList(
+                        "Dimas", "Yusuf", "Abdul", "Rega"
+                )
+        );
+
+        list.add("Test");
+        String nama = list.get(0);
+        System.out.println("nama dari index ke 0: " + nama);
+
+        for (String s : list) {
+            System.out.println("nama: " + s);
+        }
 
         Set<String> sets = new HashSet<>(Arrays.asList(
                 "Dimas", "Dimas", "Yusuf", "Yusuf", "Abdul", "Rega"
@@ -26,6 +26,18 @@ public class ArrayData {
         for (String s : sets) {
             System.out.println("nama: " + s);
         }
+        
+        Map<String, String> maps = new HashMap<>();
+        maps.put("test", "Dimas");
+        maps.put("test1", "Dimas Maryanto");
+        maps.put("test", "Dimas");
+        maps.put("test", "Dimas");
 
+        maps.forEach((key, value) -> {
+            System.out.println(
+                    "nilai key " + key +
+                            " sedangkan valuenya " + value
+            );
+        });
     }
 }
