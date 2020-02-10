a6393c115001022f435bc61b0abfdaea00f1e3f5
dimasm93 <software.dimas_m@icloud.com>
2020-02-10 11:56:47 +0700

    method

--- java-core/src/main/java/com/gmail/maryanto/dimas/TipeData.java
+++ java-core/src/main/java/com/gmail/maryanto/dimas/TipeData.java
@@ -3,61 +3,22 @@ package com.gmail.maryanto.dimas;
 
 import java.time.LocalDate;
 
+
 public class TipeData {
 
+    public static Integer hitungPajak(){
+        return 10 * 10;
+    }
+
     public static void main(String[] args) {
-        Integer umur = 20;
-        System.out.println("Umur saya adalah " + umur);
+        String namaLengkap = nama();
+        System.out.println("Nama lengkap saya adalah "+ namaLengkap);
 
-        umur = 20 + 10;
-        System.out.println("20 + 10 " + umur);
+        Integer pajak = hitungPajak();
+        System.out.println("nilai pajak "+ pajak);
+    }
 
-        umur++;
-        System.out.println("umur++ " + umur);
-
-        umur = 20 - 10;
-        System.out.println("20 - 10 " + umur);
-
-        umur = 20 * 10;
-        System.out.println("20 * 10 " + umur);
-
-        umur = 20 / 10;
-        System.out.println("0 / 10 " + umur);
-
-        umur = 20 % 10;
-        System.out.println("20 % 10 " + umur);
-
-
-        Boolean isValid = true;
-        System.out.println(
-                "Nilai dari variable isValid adalah " +
-                        (isValid ? "Benar" : "Salah")
-        );
-
-        Character aChar = 'a';
-        System.out.println(aChar);
-
-        LocalDate now = LocalDate.now().plusDays(2).plusMonths(2);
-        System.out.println("Tanggal sekarang: " + now);
-
-
-        Integer intVal = 10;
-        Float floatVal = 0f;
-        Double doubleVal = (double) intVal * floatVal;
-        System.out.println("Nilai " + intVal + " * " + floatVal + " = " + doubleVal);
-
-        Integer bil1 = 11;
-        if (bil1 == 11) {
-            System.out.println("Bilangan Sama");
-        } else if (true) {
-            System.out.println("true");
-        } else {
-            System.out.println("Bilangan tidak sama");
-        }
-
-        for (int i = 11; i > 0; i--) {
-            System.out.println("Saya berulang sebangak " + i);
-        }
-
+    public static String nama(){
+        return "Dimas Maryanto";
     }
 }
